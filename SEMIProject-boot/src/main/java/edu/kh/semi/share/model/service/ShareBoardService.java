ccb807aed88270ef4fa22ddc3d980d2d07d2e0ca
kichanwon <wgichan@gmail.com>
2025-05-20 15:07:03 +0900

    05-20 kichan

---
+++ SEMIProject-boot/src/main/java/edu/kh/semi/share/model/service/ShareBoardService.java
@@ -0,0 +1,14 @@
+package edu.kh.semi.share.model.service;
+
+import java.util.List;
+
+
+import edu.kh.semi.share.model.dto.ShareBoard;
+
+
+
+public interface ShareBoardService {
+
+	List<ShareBoard> getShareBoardList();
+
+}
