18a072139038015d8f01ca6a1301253f6bd7deff
kichanwon <wgichan@gmail.com>
2025-05-21 17:09:46 +0900

    FEAT 나눔게시판 작성중

--- SEMIProject-boot/src/main/java/edu/kh/semi/share/model/service/ShareBoardService.java
+++ SEMIProject-boot/src/main/java/edu/kh/semi/share/model/service/ShareBoardService.java
@@ -1,14 +1,23 @@
 package edu.kh.semi.share.model.service;
 
 import java.util.List;
+import java.util.Map;
 
-
+import edu.kh.semi.board.model.dto.Pagination;
 import edu.kh.semi.share.model.dto.ShareBoard;
 
-
-
 public interface ShareBoardService {
+    
+    /** 나눔 게시글 목록 조회
+     * @param paramMap
+     * @return boardList, pagination
+     */
+	Map<String, Object> selectBoardList(int boardCode, int cp);
 
-	List<ShareBoard> getShareBoardList();
+	ShareBoard selectOne(Map<String, Integer> map);
+
+	int updateReadCount(int boardNo);
+
+	int boardJJim(Map<String, Integer> map);
 
 }
