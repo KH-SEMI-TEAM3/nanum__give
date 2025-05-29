46743d911db1c455717edbffbaef5ad05f7cd77e
kichanwon <wgichan@gmail.com>
2025-05-29 16:56:30 +0900

    정렬,,,

--- SEMIProject-boot/src/main/java/edu/kh/semi/board/model/service/ShareBoardService.java
+++ SEMIProject-boot/src/main/java/edu/kh/semi/board/model/service/ShareBoardService.java
@@ -36,9 +36,6 @@ public interface ShareBoardService {
 	 */
 	Map<String, Object> searchList(Map<String, Object> paramMap, int cp);
 
-
-	List<ShareBoard> filterByCategory(Map<String, Object> paramMap);
-
 	List<Board> selectJjimList(int memberNo);
 
 }
